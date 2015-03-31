7a7608db127c3db133f67710997beb2c05034212
egeis <rcoan@callutheran.edu>
2015-03-31 10:31:34 -0700

    Changed output to assignment 4.

--- src/main/java/com/graphics/Graphics.java
+++ src/main/java/com/graphics/Graphics.java
@@ -28,22 +28,26 @@ public class Graphics {
 //         
 //         Line d = new Line(new Point(300,100, Color.RED), new Point(300,300, Color.WHITE) );
 //         d.draw();
-         
-//        Line d1 = new Line(new Point(10,10, Color.RED), new Point(10,300, Color.BLUE));
-//        Line d2 = new Line(new Point(10,300, Color.BLUE), new Point(300,300, Color.GREEN));
-//        Line d3 = new Line(new Point(10,10, Color.RED), new Point(300,300, Color.GREEN));
-//        
-//        d1.draw();
-//        d2.draw();
-//        d3.draw();
         
-         Triangle t1 = new Triangle(new Point(10,400, Color.RED), new Point(10,700, Color.RED), new Point(300,700, Color.RED));
-         t1.draw();
+        //Reflection
+         Triangle t1 = new Triangle(new Point(10,400, new Color(255,0,0,255)), new Point(10,700, new Color(255,0,0,255)), new Point(300,700, new Color(255,0,0,255)));
+         t1.draw(true);
          
-         Triangle t2 = new Triangle(new Point(10,10, Color.RED), new Point(10,300, Color.BLUE), new Point(300,300, Color.GREEN));
-         t2.draw();
+         Triangle t3 = new Triangle(new Point(301,700, new Color(255,0,0,255)), new Point(590,700, new Color(255,0,0,255)), new Point(590,400, new Color(255,0,0,255)));
+         t3.draw(true);
          
-         ImageReadWrite.ImageWrite(img, "test1.png");
+         //Multiple Colors
+         Triangle t2 = new Triangle(new Point(10,10, new Color(255,0,0,255)), new Point(10,300, new Color(0,0,255,255)), new Point(300,300, new Color(0,255,0,255)));
+         t2.draw(true);
+         
+         //Transparent Test
+         Triangle t4 = new Triangle(new Point(300,10, new Color(100,160,0,150)), new Point(300,300, new Color(100,160,0,150)), new Point(600,300, new Color(100,160,0,150)));
+         t4.draw(true);
+         
+         Triangle t5 = new Triangle(new Point(120,10, new Color(255,0,0,50)), new Point(500,300, new Color(120,0,0,50)), new Point(300,300, new Color(120,0,0,50)));
+         t5.draw(true);
+         
+         ImageReadWrite.ImageWrite(img, "graphics.png");
         
     }
     
