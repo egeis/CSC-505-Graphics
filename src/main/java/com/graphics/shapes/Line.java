a503b31a1cea2807598104cbf6435cb73950e055
egeis <rcoan@callutheran.edu>
2015-03-31 10:29:58 -0700

    Added Alpha channel calculation to line drawing.

--- src/main/java/com/graphics/shapes/Line.java
+++ src/main/java/com/graphics/shapes/Line.java
@@ -5,11 +5,9 @@ import main.java.com.graphics.shapes.utils.GraphicsObject;
 import main.java.com.graphics.shapes.utils.Point;
 
 /**
- *
+ * 
  * @author Richard Coan
  */
-
-
 public class Line extends GraphicsObject {
     public Line(Point start, Point end)
     {
@@ -28,20 +26,22 @@ public class Line extends GraphicsObject {
         
         while(true) {
             
-            double r, g, b;
+            double r, g, b, a;
             
             if(start.x == end.x)
             {
                 r = (int) ( ((float) y - start.y) / ((float) end.y - start.y)  * (end.color.getRed() - start.color.getRed()) + start.color.getRed() );
                 g = (int) ( ((float) y - start.y) / ((float) end.y - start.y)  * (end.color.getBlue() - start.color.getBlue()) + start.color.getBlue() );
                 b = (int) ( ((float) y - start.y) / ((float) end.y - start.y)  * (end.color.getGreen() - start.color.getGreen()) + start.color.getGreen() );
+                a = (int) ( ((float) y - start.y) / ((float) end.y - start.y)  * (end.color.getAlpha() - start.color.getAlpha()) + start.color.getAlpha());
             } else {
                 r = (int) ( ((float) x - start.x) / ((float) end.x - start.x)  * (end.color.getRed() - start.color.getRed()) + start.color.getRed() );
                 g = (int) ( ((float) x - start.x) / ((float) end.x - start.x)  * (end.color.getBlue() - start.color.getBlue()) + start.color.getBlue() );
                 b = (int) ( ((float) x - start.x) / ((float) end.x - start.x)  * (end.color.getGreen() - start.color.getGreen()) + start.color.getGreen() );
+                a = (int) ( ((float) x - start.x) / ((float) end.x - start.x)  * (end.color.getAlpha() - start.color.getAlpha()) + start.color.getAlpha() );
             }
        
-            Color color = new Color((int) r, (int) g, (int) b);
+            Color color = new Color((int) r, (int) g, (int) b, (int) a);
             
             points.add(new Point(x,y, color));
             if(x == end.x && y == end.y) break;
