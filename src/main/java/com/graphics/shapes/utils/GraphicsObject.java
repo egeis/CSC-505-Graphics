341a0e4f75ca5ff26504c1ab31936d52346be8b4
egeis <rcoan@callutheran.edu>
2015-03-31 10:29:23 -0700

    Updated fill creation to be handled by Abstract shape.

--- src/main/java/com/graphics/shapes/utils/GraphicsObject.java
+++ src/main/java/com/graphics/shapes/utils/GraphicsObject.java
@@ -1,28 +1,74 @@
 package main.java.com.graphics.shapes.utils;
 
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import main.java.com.graphics.Graphics;
+import main.java.com.graphics.shapes.Line;
 
 /**
- *
+ * Parent Abstract Shape Class to all shapes.
+ * Handle drawing of the shape object.
  * @author Richard Coan
  */
-
-
 public abstract class GraphicsObject {
     
-    protected Set<Point> points = new HashSet<Point>();
+    protected Set<Point> points = new HashSet();
 
     public Set<Point> getPoints() {
         return points;
     }
     
+    public void draw(boolean fill)
+    {
+       if(fill)
+       {
+            Set<Point> completed = new HashSet();
+            
+            while(points.size() > 0)
+            {
+                Iterator iter = points.iterator();
+                Point s1 = (Point) iter.next();
+                points.remove(s1);
+
+                Point s2 = null;
+
+                boolean sP = false;
+                for(Point p : points)
+                {
+                    if(s1.y == p.y)
+                    {
+                        s2 = p;
+                        //points.remove(p);
+                        sP = true;
+                        break;
+                    }
+                }
+
+                if(sP)
+                {
+                    completed.addAll(new Line(s1, s2).getPoints());
+                } else {
+                    System.out.println(s1 + " : was alone...");
+                    completed.add(s1);
+                }
+            }
+
+            points.clear();
+            points.addAll(completed);
+       }
+       
+       this.draw();
+    }
+    
     public void draw()
     {        
         int t = 0;
+        double r, g, b, a;
+        Color color;
+        
+        System.out.println(Graphics.img[3][300][300]);
         
         for(Point p : points) {
             //Handles Out of Bounds
@@ -32,24 +78,28 @@ public abstract class GraphicsObject {
             //Handles Depth (objects drawn from closest first).
             if( Graphics.img[3][p.y][p.x] == 255) 
             {
-                System.out.println("a");
+                System.out.println(p + " : is already used...");
                 continue;
+            } else if(Graphics.img[3][p.y][p.x] != 0)
+            {
+                //Blending the old color with the new color.
+                //Source for blending: http://www.java2s.com/Code/Java/2D-Graphics-GUI/Blendtwocolors.htm
+                double totalAlpha = Graphics.img[3][p.y][p.x] + p.color.getAlpha();
+                double weight0 =  Graphics.img[3][p.y][p.x] / totalAlpha;
+                double weight1 =  p.color.getAlpha() / totalAlpha;
+
+                r = weight0 *  Graphics.img[0][p.y][p.x] + weight1 * p.color.getRed();
+                g = weight0 *  Graphics.img[1][p.y][p.x] + weight1 * p.color.getGreen();
+                b = weight0 *  Graphics.img[2][p.y][p.x] + weight1 * p.color.getBlue();
+                a = Math.max( Graphics.img[3][p.y][p.x], p.color.getAlpha());
+                
+                if(a == 0) System.out.println("error");
+                
+                color = new Color((int) r, (int) g, (int) b, (int) a);
+            } else {
+                color = p.color;
             }
-                       
-            //Blending the old color with the new color.
-//            double totalAlpha = Graphics.img[3][p.y][p.x] + p.color.getAlpha();
-//            double weight0 =  Graphics.img[3][p.y][p.x] / totalAlpha;
-//            double weight1 =  Graphics.img[3][p.y][p.x] / totalAlpha;
-//
-//            double r = weight0 *  Graphics.img[0][p.y][p.x] + weight1 * p.color.getRed();
-//            double g = weight0 *  Graphics.img[1][p.y][p.x] + weight1 * p.color.getGreen();
-//            double b = weight0 *  Graphics.img[2][p.y][p.x] + weight1 * p.color.getBlue();
-//            double a = Math.max( Graphics.img[3][p.y][p.x], p.color.getAlpha());
-//            
-//            Color color = new Color((int) r, (int) g, (int) b, (int) a);
-            
-            Color color = p.color;
-            
+
             //Setting the color data to the image.
             Graphics.img[0][p.y][p.x] = color.getRed();
             Graphics.img[1][p.y][p.x] = color.getGreen();
